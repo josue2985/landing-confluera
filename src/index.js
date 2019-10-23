09fa8f08e46aff8a9c19a86b1b222b74895a19aa
josue2985 <jbrito@opstrax.com>
2019-10-23 14:26:38 -0500

    new changes index

--- src/index.js
+++ src/index.js
@@ -1,26 +1,19 @@
 import "./assets/scss/global.scss";
-import ScrollMagic from 'scrollmagic';
 import $ from 'jquery';
+// import ScrollMagic from 'scrollmagic';
 import { TweenMax, Power3, Expo } from "gsap";
+import * as ScrollMagic from 'scrollmagic';
 
-// TweenMax.to("h1", 1, { x:100 });
+const controller = new ScrollMagic.Controller();
+const long = document.getElementById('mevoy');
 
-window.onload = function() {
-    document.getElementById("intro-laptop").addEventListener("ended",myHandler,false);
-    function myHandler(e) {
-        // document.getElementById("intro-section").classList.add("show");
-        TweenMax.to(".intro-laptop", 1, {opacity: 0.5, ease: Expo.easeOut});
-        TweenMax.to(".intro-title-line1", 0.8, {opacity: 1, y:0, ease: Expo.easeOut});
-        TweenMax.to(".intro-title-line2", 1.5, {opacity: 1, y:0, delay:0.3, ease: Expo.easeOut});
-    };
-}
-
-// $(document).ready(function () {
-//         var introVideo = document.getElementById("intro-laptop");
-//         document.getElementById("intro-laptop").play();
-// });
-
-
+const myScene = new ScrollMagic.Scene({
+    triggerElement: long,
+    triggerHook: "onLeave"
+})
+//.setClassToggle('#features-approach', 'chao')
+.setClassToggle('#intro-laptop', 'show')
+.addTo(controller);
 
 
 new IntersectionObserver(function(e, o) {
@@ -30,7 +23,6 @@ new IntersectionObserver(function(e, o) {
     document.documentElement.setAttribute("class", "stuck");
   }
 }).observe(document.querySelector(".trigger"));
-
 
 $(document).ready(function () {
     var timeInterval, tabCount = 0, currnetIndex = 1;
@@ -70,24 +62,69 @@ $(document).ready(function () {
     });
 });
 
-//$(function() {
+var TxtRotate = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  };
+  
+TxtRotate.prototype.tick = function() {
+var i = this.loopNum % this.toRotate.length;
+var fullTxt = this.toRotate[i];
 
-    // cache a reference to the tabs
-    //var tabs = $('#myTab li a');
+if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+} else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+}
 
-     //$( "#myTab li a" ).first().addClass( "active show" );
+this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
-    //on click to tab, turn it on, and turn previously-on tab off
-    // tabs.click(function() { $(this).addClass('active show').siblings('.show').removeClass('active show'); });
+var that = this;
+var delta = 300 - Math.random() * 100;
 
-    //auto-rotate every 5 seconds
-    // setInterval(function() {
+if (this.isDeleting) { delta /= 2; }
 
-    //         //get currently-on tab
-    //     var onTab = tabs.filter('.active');
+if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+} else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+}
 
-    //         //click either next tab, if exists, else first one
-    //     var nextTab = onTab.index() < tabs.length-1 ? onTab.next() : tabs.first();
-    //     nextTab.click();
-    // }, 5000);
-//});
+setTimeout(function() {
+    that.tick();
+}, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-rotate');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+        new TxtRotate(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    document.body.appendChild(css);
+
+    document.getElementById("intro-laptop").addEventListener("ended",myHandler,false);
+    function myHandler(e) {
+        // document.getElementById("intro-section").classList.add("show");
+        TweenMax.to(".intro-laptop", 1, {opacity: 0.5, ease: Expo.easeOut});
+        TweenMax.to(".intro-title-line1", 0.8, {opacity: 1, y:0, ease: Expo.easeOut});
+        TweenMax.to(".intro-title-line2", 1.5, {opacity: 1, y:0, delay:0.3, ease: Expo.easeOut});
+    };
+};
+
+
